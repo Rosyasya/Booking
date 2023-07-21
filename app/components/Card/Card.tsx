6803446f08a145e95f45dc9a617fae3193097f7b
Ростиslave <opamkgue@ukr.net>
2023-07-21 17:48:28 +0300

    css fix

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import styles from './Card.module.scss'
+import styles from './style.module.scss'
 
 const Card = () => {
     return (
@@ -11,58 +11,58 @@ const Card = () => {
             <h2 className={styles.title}>Guy Hawkins</h2>
 
             <div className={styles.info}>
-                <div className={styles.infoContainer}>
+                <div className={styles.info__container}>
                     <h5>Type room</h5>
                     <h4>Superior</h4>
                 </div>
-                <div className={styles.infoContainer}>
+                <div className={styles.info__container}>
                     <h5>Room #</h5>
                     <h4>202</h4>
                 </div>
-                <div className={styles.infoContainer}>
+                <div className={styles.info__container}>
                     <h5>Guests</h5>
                     <h4>2</h4>
                 </div>
-                <div className={styles.infoContainer}>
+                <div className={styles.info__container}>
                     <h5>Payment method</h5>
                     <h4>Credit card</h4>
                 </div>
-                <div className={styles.infoContainer}>
+                <div className={styles.info__container}>
                     <h5>Source</h5>
                     <h4>Reception</h4>
                 </div>
             </div>
 
             <div className={styles.date}>
-                <div className={styles.dateContainer}>
+                <div className={styles.date__container}>
                     <h4>9-9-2022</h4>
                     <h4>1:00 PM</h4>
                 </div>
-                <div className={styles.dateContainer}>
+                <div className={styles.date__container}>
                     <h4>4</h4>
                     <h4>nights</h4>
                 </div>
-                <div className={styles.dateContainer}>
+                <div className={styles.date__container}>
                     <h4>13-9-2022</h4>
                     <h4>11:00 PM</h4>
                 </div>
             </div>
 
             <div className={styles.price}>
-                <div className={styles.priceContainer}>
-                    <h4 className={styles.priceContainerTitle}>Sub Total</h4>
+                <div className={styles.price__container}>
+                    <h4 className={styles.price__title}>Sub Total</h4>
                     <h4>$800</h4>
                 </div>
-                <div className={styles.priceContainer}>
-                    <h4 className={styles.priceContainerTitle}>Advance Payment</h4>
+                <div className={styles.price__container}>
+                    <h4 className={styles.price__title}>Advance Payment</h4>
                     <h4>$100</h4>
                 </div>
-                <div className={styles.priceContainer}>
-                    <h4 className={styles.priceContainerTitle}>Discount</h4>
+                <div className={styles.price__container}>
+                    <h4 className={styles.price__title}>Discount</h4>
                     <h4>$50</h4>
                 </div>
-                <div className={styles.priceContainer}>
-                    <h4 className={styles.priceContainerTitle}>Total</h4>
+                <div className={styles.price__container}>
+                    <h4 className={styles.price__title}>Total</h4>
                     <h4>$700</h4>
                 </div>
             </div>
